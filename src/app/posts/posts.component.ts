1e6018da77c8730d9f409e4657a4a49015f6b55d
vqkhanh2009@gmail.com <vqkhanh2009@gmail.com>
2022-05-26 15:19:10 +0700

    107 post data

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -9,11 +9,28 @@ import { Component, OnInit } from '@angular/core';
 export class PostsComponent implements OnInit {
 
   posts: [] | any; 
+  private url = 'https://jsonplaceholder.typicode.com/posts';
 
-  constructor(httpClient: HttpClient) { 
-    httpClient.get('https://jsonplaceholder.typicode.com/posts')
+  constructor(private httpClient: HttpClient) { 
+    httpClient.get(this.url)
       .subscribe(res => {
         this.posts = res;
+      })
+
+      
+  }
+
+  createPost(input: HTMLInputElement){
+
+    let post: any = {};
+    post['title'] = input.value;
+    input.value = '';
+
+    this.httpClient.post(this.url, JSON.stringify(post))
+      .subscribe( (res: any)  => {
+        post['id'] = res.id;
+        this.posts.splice(0, 0, post);
+        // this.posts.unshift(post);
       })
   }
 
